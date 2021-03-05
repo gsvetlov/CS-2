c0582138fbed75866b1efed389f5bee92b646a0b
SvGA <gsvetlov@gmail.com>
2021-03-05 23:30:52 +0300

    general homework02

---
+++ HomeTask02/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * 1. Построить три класса (базовый и 2 потомка), описывающих некоторых работников
+ * с почасовой оплатой (один из потомков) и фиксированной оплатой (второй потомок).
+ *  а) Описать в базовом классе абстрактный метод для расчёта среднемесячной заработной платы. 
+ *     Для «повременщиков» формула для расчета такова: «среднемесячная заработная плата = 20.8 * 8 * почасовая ставка»,
+ *     для работников с фиксированной оплатой «среднемесячная заработная плата = фиксированная месячная оплата».
+ *  б) Создать на базе абстрактного класса массив сотрудников и заполнить его.
+ *  в) *Реализовать интерфейсы для возможности сортировки массива, используя Array.Sort().
+ *  г) *Создать класс, содержащий массив сотрудников, и реализовать возможность вывода данных с использованием foreach.
+ */
+
+using System;
+
+namespace HomeTask02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseEmployee[] employees = new BaseEmployee[]
+            {
+                new HourlyEmployee("Jack", 12.0m),
+                new HourlyEmployee("John", 25.0m),
+                new HourlyEmployee("Sue", 30.0m),
+                new SalaryEmployee("Frank", 3000m),
+                new SalaryEmployee("Mary", 5000m),
+                new SalaryEmployee("Jeremy", 2500m)
+            };
+            Array.Sort(employees);
+            Console.WriteLine("Employees\nSorted by natural order");
+            foreach (var person in employees)
+                Console.WriteLine(person);
+
+            Array.Sort(employees, new EmployeeCompareByByWages());
+
+            EmployeeCollection collection = new EmployeeCollection(employees);
+            Console.WriteLine("\nSorted by wages");
+            foreach (var person in collection)
+                Console.WriteLine(person);
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ HomeTask02/SalaryEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HomeTask02
+{
+    class SalaryEmployee : BaseEmployee
+    {
+        public decimal Salary { get; }
+        public SalaryEmployee(string name, decimal salary) : base(name)
+        {
+            Salary = salary > 0 ? salary : throw new ArgumentOutOfRangeException(nameof(salary));
+        }
+        public override decimal GetAvgMonthlyWages() => Salary;
+        public override string ToString() => $"{base.ToString()}\tSalary {Salary:F2}\tMonthly {GetAvgMonthlyWages():F2}";
+    }
+}
