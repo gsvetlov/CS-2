0eb973eb172d96d52cfca6b625fa1be22193597b
SvGA <gsvetlov@gmail.com>
2021-03-06 16:47:42 +0300

    Added Space body collisions and validity checks, removal of dead objects, code refactoring

---
+++ Asteroid/GameObjectException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Asteroid
+{
+    [System.Serializable]
+    public class GameObjectException : Exception
+    {
+        public GameObjectException() { }
+        public GameObjectException(string message) : base(message) { }
+        public GameObjectException(string message, Exception inner) : base(message, inner) { }
+        protected GameObjectException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+
+}
---
+++ Asteroid/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asteroid
+{
+    static class Utils
+    {
+        private static Random random = new Random();
+        public static int Random(int max) => random.Next(max);
+        public static int Random(int min, int max) => random.Next(min, max);
+        public static Point RandomPos(int width, int height) => new Point(random.Next(width), random.Next(height));
+        public static Point RandomDir(int min, int max, bool doubleSide = false)
+        {
+            int x;
+            int y;
+            if (doubleSide)
+            {
+                x = GetSides();
+                y = GetSides();
+            }
+            else
+            {
+                x = random.Next(min, max);
+                y = random.Next(min, max);
+            }
+
+            return new Point(x, y);
+
+            int GetSides()
+            {
+                int v = random.Next(-max, max);
+                if (v < min && v > -min) return min;
+                return v;
+            }
+        }
+    }
+}
