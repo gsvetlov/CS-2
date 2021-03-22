be98f3bd342480592a798d9ca171affe43d6e243
Георгий Светлов <gsvetlov@mail.ru>
2021-03-22 16:39:43 +0300

    base structure

---
+++ EmployeeView.Data/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeView.Data
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public Department Department { get; set; }
+    }
+}
